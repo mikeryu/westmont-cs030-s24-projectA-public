d43591f5c4877965b2bd4bec710e7678aac8266a
Mike Ryu <mryu@westmont.edu>
2024-01-17 01:00:43 -0800

    Initial commit with starter code.

---
+++ SpringBreakDriver.java
@@ -0,0 +1,15 @@
+/**
+ * Westmont College Spring 2024
+ * CS 030 Project A
+ *
+ * @author Assistant Professor Mike Ryu mryu@westmont.edu
+ * @author Boaty McBoatface bmcboatface@westmont.edu
+ */
+
+public class SpringBreakDriver {
+
+  private static final int EXIT_CODE_NOT_ENOUGH_ARGS = 1;
+  private static final int EXIT_CODE_FILE_OPEN_ERROR = 2;
+  private static final int EXIT_CODE_FILE_READ_ERROR = 3;
+
+}
